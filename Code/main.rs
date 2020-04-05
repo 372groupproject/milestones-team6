7cace089e9ee56a0ece903583f6a8c714e3f4f25
louisgalluzzi <42557043+louisgalluzzi@users.noreply.github.com>
2020-04-04 22:21:10 -0700

    Add files via upload

--- Code/main.rs
+++ Code/main.rs
@@ -1,6 +1,28 @@
-mod testing;
+//mod testing;
+mod p2_if;
+mod p2_if_else;
+mod p2_if_let;
+mod p2_loop;
+mod p2_while;
+mod p2_return_loop;
+mod p2_for;
+mod p2_match;
+mod p2_panic;
+mod p2_result;
 
 fn main(){
-    println!("Hello World");
-    testing::main();
+    //println!("Hello World");
+    //testing::main();
+    p2_if::main();
+    //p2_if_else::main();
+    //p2_if_let::main();
+    //p2_loop::main();
+    //p2_while::main();
+    //p2_return_loop::main();
+    //p2_for::main();
+    //p2_match::main();
+    //p2_panic::main();
+    //p2_result::main();
+
+
 }
